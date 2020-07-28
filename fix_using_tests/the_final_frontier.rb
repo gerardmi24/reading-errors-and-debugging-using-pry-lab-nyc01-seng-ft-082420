7dc7eb1cdf6a0e6f4b5f48766d1eef0842b1e64f
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-28 01:59:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,10 +1,10 @@
 # don't forget to add: require 'pry'
-date = generate_star_date
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-
+date = generate_star_date
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
